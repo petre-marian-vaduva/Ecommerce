54dbcc7e3d8e57251b9f12dcfc890b787649b081
Petre <81445716+petre-marian-vaduva@users.noreply.github.com>
2022-04-17 02:20:29 +0100

    Update views.py

--- project1/store/views.py
+++ project1/store/views.py
@@ -6,7 +6,7 @@ def store(request):
     return render(request, 'store/store.html')
 
 def cart(request):
-    return render(request, 'store/store.html')
+    return render(request, 'store/cart.html')
 
 def checkout(request):
-    return render(request, 'store/store.html')
+    return render(request, 'store/checkout.html')
