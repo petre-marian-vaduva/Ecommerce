88bb1bd404721355c1f24c75c718c2ddd5bd12d7
Petre <81445716+petre-marian-vaduva@users.noreply.github.com>
2022-07-03 21:10:44 +0100

    Update models.py

--- project1/store/models.py
+++ project1/store/models.py
@@ -26,3 +26,9 @@ class Order(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
