6b8f0db5eaf4b19222fc22e7661e82e671d25162
Petre <81445716+petre-marian-vaduva@users.noreply.github.com>
2022-07-25 11:07:56 +0100

    update store models

--- project1/store/models.py
+++ project1/store/models.py
@@ -20,6 +20,13 @@ class Product(models.Model):
     def __str__(self) -> str:
         return self.name
 
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, blank=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
